8b9b907413926a0ebd048fc66d7b2854e16178ab
Patrick Pucan <patrickp@walkingco.com>
2025-02-27 18:58:25 +0800

    Frontend Setup

---
+++ webpack.mix.js
@@ -0,0 +1,16 @@
+let mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+
+// Main theme files
+// mix.js('src/js/lookbook.js', 'assets/')
+//    .sass('src/scss/lookbook.scss', 'assets/')
+//    .options({
+//      processCssUrls: false,
+//      hmrOptions: {
+//        https: false,
+//        host: 'localhost',
+//        port: '8080'
+//      }
+//    });
+
+// Add here the components you want to compile separately
