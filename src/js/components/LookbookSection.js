aa481c181abe8cb7b2e114c4eed355b788a8db47
Patrick Pucan <patrickp@walkingco.com>
2025-02-27 22:02:59 +0800

    Lookbook

---
+++ src/js/components/LookbookSection.js
@@ -0,0 +1,41 @@
+export default class LookbookSection extends HTMLElement {
+  constructor() {
+    super();
+    this.initializeSlider();
+  }
+
+  initializeSlider() {
+    const slider = this.querySelector('.lookbook-slider');
+    if (!slider) return;
+    
+    // Add touch and mouse drag scrolling functionality
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 2;
+      slider.scrollLeft = scrollLeft - walk;
+    });
+  }
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,15 +2,15 @@ let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 
 // Main theme files
-// mix.js('src/js/lookbook.js', 'assets/')
-//    .sass('src/scss/lookbook.scss', 'assets/')
-//    .options({
-//      processCssUrls: false,
-//      hmrOptions: {
-//        https: false,
-//        host: 'localhost',
-//        port: '8080'
-//      }
-//    });
+mix.js('src/js/lookbook.js', 'assets/')
+   .sass('src/scss/sections/lookbook.scss', 'assets/')
+   .options({
+     processCssUrls: false,
+     hmrOptions: {
+       https: false,
+       host: 'localhost',
+       port: '8080'
+     }
+   });
 
 // Add here the components you want to compile separately
